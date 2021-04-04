68824e759a819850f86090b4e84d8054d46250aa
Юлий Бондарь <trickyshot@mail.ru>
2021-04-04 15:22:46 +0300

    Values renamed for better visuality

--- src/dvvset.erl
+++ src/dvvset.erl
@@ -5,8 +5,8 @@
 %%% File:      dvvset.erl
 %%%
 %%% @title Dotted Version Vector Set
-%%% @author    Ricardo Tomé Gonçalves <tome.wave@gmail.com>
-%%% @author    Paulo Sérgio Almeida <pssalmeida@gmail.com>
+%%% @author    Ricardo Tomï¿½ Gonï¿½alves <tome.wave@gmail.com>
+%%% @author    Paulo Sï¿½rgio Almeida <pssalmeida@gmail.com>
 %%%
 %%% @copyright The MIT License (MIT)
 %%% Copyright (C) 2013
@@ -78,89 +78,89 @@
 %% @doc Constructs a new clock set without causal history,
 %% and receives one value that goes to the anonymous list.
 -spec new(value()) -> clock().
-new(V) -> {[], [V]}.
+new(Value) -> {[], [Value]}.
 
 %% @doc Same as new/1, but receives a list of values, instead of a single value.
 -spec new_list([value()]) -> clock().
-new_list(Vs) when is_list(Vs) -> {[], Vs};
-new_list(V) -> {[], [V]}.
+new_list(Values) when is_list(Values) -> {[], Values};
+new_list(Value) -> {[], [Value]}.
 
 %% @doc Constructs a new clock set with the causal history
 %% of the given version vector / vector clock,
 %% and receives one value that goes to the anonymous list.
 %% The version vector SHOULD BE the output of join/1.
 -spec new(vector(), value()) -> clock().
-new(VV, V) ->
+new(VV, Value) ->
     VVS = lists:sort(VV), % defense against non-order preserving serialization
-    {[{I, N, []} || {I, N} <- VVS], [V]}.
+    {[{Id, Counter, []} || {Id, Counter} <- VVS], [Value]}.
 
 %% @doc Same as new/2, but receives a list of values, instead of a single value.
 -spec new_list(vector(), [value()]) -> clock().
-new_list(VV, Vs) when is_list(Vs) ->
+new_list(VV, Values) when is_list(Values) ->
     VVS = lists:sort(VV), % defense against non-order preserving serialization
-    {[{I, N, []} || {I, N} <- VVS], Vs};
-new_list(VV, V) -> new_list(VV, [V]).
+    {[{Id, Counter, []} || {Id, Counter} <- VVS], Values};
+new_list(VV, Value) -> new_list(VV, [Value]).
 
 %% @doc Synchronizes a list of clocks using sync/2.
 %% It discards (causally) outdated values, 
 %% while merging all causal histories.
 -spec sync([clock()]) -> clock().
-sync(L) -> lists:foldl(fun sync/2, {}, L).
+sync(ListClocks) -> lists:foldl(fun sync/2, {}, ListClocks).
 
 %% Private function
 -spec sync(clock(), clock()) -> clock().
-sync({}, C) -> C;
-sync(C ,{}) -> C;
-sync(C1={E1,V1},C2={E2,V2}) ->
-    V = case less(C1,C2) of
-        true  -> V2; % C1 < C2 => return V2
-        false -> case less(C2,C1) of
-                    true  -> V1; % C2 < C1 => return V1
+sync({}, Clock) -> Clock;
+sync(Clock ,{}) -> Clock;
+sync(Clock1={Entries1,Values1},Clock2={Entries2,Values2}) ->
+    Value = case less(Clock1,Clock2) of
+        true  -> Values2; % Clock1 < Clock2 => return Values2
+        false -> case less(Clock2,Clock1) of
+                    true  -> Values1; % Clock2 < Clock1 => return Values1
                     false -> % keep all unique anonymous values and sync entries()
-                        sets:to_list(sets:from_list(V1++V2))
+                        sets:to_list(sets:from_list(Values1++Values2))
                  end
     end,
-    {sync2(E1,E2),V}.
+    {sync2(Entries1,Entries2),Value}.
 
 %% Private function
 -spec sync2(entries(), entries()) -> entries().
-sync2([], C) -> C;
-sync2(C, []) -> C;
-sync2([{I1, N1, L1}=H1 | T1]=C1, [{I2, N2, L2}=H2 | T2]=C2) ->
+sync2([], Clock) -> Clock;
+sync2(Clock, []) -> Clock;
+sync2([{Id1, Counter1, Values1}=H1 | Entries1]=Clock1, [{Id2, Counter2, Values2}=H2 | Entries2]=Clock2) ->
     if
-      I1 < I2 -> [H1 | sync2(T1, C2)];
-      I1 > I2 -> [H2 | sync2(T2, C1)];
-      true    -> [merge(I1, N1, L1, N2, L2) | sync2(T1, T2)]
+      Id1 < Id2 -> [H1 | sync2(Entries1, Clock2)];
+      Id1 > Id2 -> [H2 | sync2(Entries2, Clock1)];
+      true    -> [merge(Id1, Counter1, Values1, Counter2, Values2) | sync2(Entries1, Entries2)]
     end.
 
 %% Private function
 -spec merge(id(), counter(), values(), counter(), values()) -> {id(), counter(), values()}.
-merge(I, N1, L1, N2, L2) ->
-    LL1 = length(L1),
-    LL2 = length(L2),
-    case N1 >= N2 of
+merge(Id, Counter1, Values1, Counter2, Values2) ->
+    LL1 = length(Values1),
+    LL2 = length(Values2),
+    case Counter1 >= Counter2 of
         true ->
-          case N1 - LL1 >=  N2 - LL2 of 
-            true  -> {I, N1, L1};
-            false -> {I, N1, lists:sublist(L1, N1 - N2 + LL2)}
+          case Counter1 - LL1 >=  Counter2 - LL2 of 
+            true  -> {Id, Counter1, Values1};
+            false -> {Id, Counter1, lists:sublist(Values1, Counter1 - Counter2 + LL2)}
           end;
         false ->
-          case N2 - LL2 >=  N1 - LL1 of 
-            true  -> {I, N2, L2};
-            false -> {I, N2, lists:sublist(L2, N2 - N1 + LL1)}
+          case Counter2 - LL2 >=  Counter1 - LL1 of 
+            true  -> {Id, Counter2, Values2};
+            false -> {Id, Counter2, lists:sublist(Values2, Counter2 - Counter1 + LL1)}
           end
     end.
 
 
 %% @doc Return a version vector that represents the causal history.
 -spec join(clock()) -> vector().
-join({C,_}) -> [{I, N} || {I, N, _} <- C].
+join({Clock,_}) -> [{Id, Counter} || {Id, Counter, _} <- Clock].
 
 %% @doc Advances the causal history with the given id.
 %% The new value is the *anonymous dot* of the clock.
 %% The client clock SHOULD BE a direct result of new/2.
 -spec update(clock(), id()) -> clock().
-update({C,[V]}, I) -> {event(C, I, V), []}.
+update({Clock,[Value]}, Id) -> {event(Clock, Id, Value), []}.
 
 %% @doc Advances the causal history of the
 %% first clock with the given id, while synchronizing
@@ -172,139 +172,139 @@ update({C,[V]}, I) -> {event(C, I, V), []}.
 %% the new value in the *anonymous dot* while
 %% the second clock is from the local server.
 -spec update(clock(), clock(), id()) -> clock().
-update({Cc,[V]}, Cr, I) ->
+update({Cc,[Value]}, Cr, Id) ->
     %% Sync both clocks without the new value
-    {C,Vs} = sync({Cc,[]}, Cr),
+    {Clock,Values} = sync({Cc,[]}, Cr),
     %% We create a new event on the synced causal history,
-    %% with the id I and the new value.
+    %% with the id Id and the new value.
     %% The anonymous values that were synced still remain.
-    {event(C, I, V), Vs}.
+    {event(Clock, Id, Value), Values}.
 
 %% Private function
 -spec event(vector(), id(), value()) -> entries().
-event([], I, V) -> [{I, 1, [V]}];
-event([{I, N, L} | T], I, V) -> [{I, N+1, [V | L]} | T];
-event([{I1, _, _} | _]=C, I, V) when I1 > I -> [{I, 1, [V]} | C];
-event([H | T], I, V) -> [H | event(T, I, V)].
+event([], Id, Value) -> [{Id, 1, [Value]}];
+event([{Id, Counter, ListClocks} | T], Id, Value) -> [{Id, Counter+1, [Value | ListClocks]} | T];
+event([{Id1, _, _} | _]=Clock, Id, Value) when Id1 > Id -> [{Id, 1, [Value]} | Clock];
+event([H | T], Id, Value) -> [H | event(T, Id, Value)].
 
 %% @doc Returns the total number of values in this clock set.
 -spec size(clock()) -> non_neg_integer().
-size({C,Vs}) -> lists:sum([length(L) || {_,_,L} <- C]) + length(Vs).
+size({Clock,Values}) -> lists:sum([length(ListClocks) || {_,_,ListClocks} <- Clock]) + length(Values).
 
 %% @doc Returns all the ids used in this clock set.
 -spec ids(clock()) -> [id()].
-ids({C,_}) -> ([I || {I,_,_} <- C]).
+ids({Clock,_}) -> ([Id || {Id,_,_} <- Clock]).
 
 %% @doc Returns all the values used in this clock set,
 %% including the anonymous values.
 -spec values(clock()) -> [value()].
-values({C,Vs}) -> Vs ++ lists:append([L || {_,_,L} <- C]).
+values({Clock,Values}) -> Values ++ lists:append([ListClocks || {_,_,ListClocks} <- Clock]).
 
 %% @doc Compares the equality of both clocks, regarding
 %% only the causal histories, thus ignoring the values.
 -spec equal(clock() | vector(), clock() | vector()) -> boolean().
-equal({C1,_},{C2,_}) -> equal2(C1,C2); % DVVSet
-equal(C1,C2) when is_list(C1) and is_list(C2) -> equal2(C1,C2). %vector clocks
+equal({Clock1,_},{Clock2,_}) -> equal2(Clock1,Clock2); % DVVSet
+equal(Clock1,Clock2) when is_list(Clock1) and is_list(Clock2) -> equal2(Clock1,Clock2). %vector clocks
 
 %% Private function
 -spec equal2(vector(), vector()) -> boolean().
 equal2([], []) -> true;
-equal2([{I, C, L1} | T1], [{I, C, L2} | T2]) 
-    when length(L1) =:= length(L2) -> 
-    equal2(T1, T2);
+equal2([{Id, Clock, Values1} | Entries1], [{Id, Clock, Values2} | Entries2]) 
+    when length(Values1) =:= length(Values2) -> 
+    equal2(Entries1, Entries2);
 equal2(_, _) -> false.
 
 %% @doc Returns True if the first clock is causally older than
 %% the second clock, thus values on the first clock are outdated.
 %% Returns False otherwise.
 -spec less(clock(), clock()) -> boolean().
-less({C1,_}, {C2,_}) -> greater(C2, C1, false).
+less({Clock1,_}, {Clock2,_}) -> greater(Clock2, Clock1, false).
 
 %% Private function
 -spec greater(vector(), vector(), boolean()) -> boolean().
 greater([], [], Strict) -> Strict;
 greater([_|_], [], _) -> true;
 greater([], [_|_], _) -> false;
-greater([{I, N1, _} | T1], [{I, N2, _} | T2], Strict) ->
+greater([{Id, Counter1, _} | Entries1], [{Id, Counter2, _} | Entries2], Strict) ->
    if
-     N1 == N2 -> greater(T1, T2, Strict);
-     N1 >  N2 -> greater(T1, T2, true);
-     N1 <  N2 -> false
+     Counter1 == Counter2 -> greater(Entries1, Entries2, Strict);
+     Counter1 >  Counter2 -> greater(Entries1, Entries2, true);
+     Counter1 <  Counter2 -> false
    end;
-greater([{I1, _, _} | T1], [{I2, _, _} | _]=C2, _) when I1 < I2 -> greater(T1, C2, true);
+greater([{Id1, _, _} | Entries1], [{Id2, _, _} | _]=Clock2, _) when Id1 < Id2 -> greater(Entries1, Clock2, true);
 greater(_, _, _) -> false.
 
 %% @doc Maps (applies) a function on all values in this clock set,
 %% returning the same clock set with the updated values.
 -spec map(fun((value()) -> value()), clock()) -> clock().
-map(F, {C,Vs}) -> 
-    {[ {I, N, lists:map(F, V)} || {I, N, V} <- C], lists:map(F, Vs)}.
+map(F, {Clock,Values}) -> 
+    {[ {Id, Counter, lists:map(F, Value)} || {Id, Counter, Value} <- Clock], lists:map(F, Values)}.
 
 
 %% @doc Return a clock with the same causal history, but with only one
 %% value in the anonymous placeholder. This value is the result of
 %% the function F, which takes all values and returns a single new value.
 -spec reconcile(Winner::fun(([value()]) -> value()), clock()) -> clock().
-reconcile(F, C) ->
-    V = F(values(C)),
-    new(join(C), V).
+reconcile(F, Clock) ->
+    Value = F(values(Clock)),
+    new(join(Clock), Value).
 
 %% @doc Returns the latest value in the clock set,
 %% according to function F(A,B), which returns *true* if 
 %% A compares less than or equal to B, false otherwise.
 -spec last(LessOrEqual::fun((value(),value()) -> boolean()), clock()) -> value().
-last(F, C) ->
-   {_ ,_ , V2} = find_entry(F, C),
-   V2.
+last(F, Clock) ->
+   {_ ,_ , Values2} = find_entry(F, Clock),
+   Values2.
 
 %% @doc Return a clock with the same causal history, but with only one
 %% value in its original position. This value is the newest value
 %% in the given clock, according to function F(A,B), which returns *true*
 %% if A compares less than or equal to B, false otherwise.
 -spec lww(LessOrEqual::fun((value(),value()) -> boolean()), clock()) -> clock().
-lww(F, C={E,_}) ->
-    case find_entry(F, C) of
-        {id, I, V}      -> {join_and_replace(I, V, E),[]};
-        {anonym, _, V}  -> new(join(C), V)
+lww(F, Clock={E,_}) ->
+    case find_entry(F, Clock) of
+        {id, Id, Value}      -> {join_and_replace(Id, Value, E),[]};
+        {anonym, _, Value}  -> new(join(Clock), Value)
     end.
 
 %% find_entry/2 - Private function
-find_entry(F, {[], [V|T]}) -> find_entry(F, null, V, {[],T}, anonym);
-find_entry(F, {[{_, _, []} | T], Vs}) -> find_entry(F, {T,Vs});
-find_entry(F, {[{I, _, [V|_]} | T], Vs}) -> find_entry(F, I, V, {T,Vs}, id).
+find_entry(F, {[], [Value|T]}) -> find_entry(F, null, Value, {[],T}, anonym);
+find_entry(F, {[{_, _, []} | T], Values}) -> find_entry(F, {T,Values});
+find_entry(F, {[{Id, _, [Value|_]} | T], Values}) -> find_entry(F, Id, Value, {T,Values}, id).
 
 %% find_entry/5 - Private function
-find_entry(F, I, V, C, Flag) ->
+find_entry(F, Id, Value, Clock, Flag) ->
     Fun = fun (A,B) ->
         case F(A,B) of
             false -> {left,A}; % A is newer than B
             true  -> {right,B} % A is older than B
         end
     end,
-    find_entry2(Fun, I, V, C, Flag).
+    find_entry2(Fun, Id, Value, Clock, Flag).
 
 %% find_entry2/5 - Private function
-find_entry2(_, I, V, {[], []}, anonym) -> {anonym, I , V};
-find_entry2(_, I, V, {[], []}, id) -> {id, I, V};
-find_entry2(F, I, V, {[], [V1 | T]}, Flag) ->
-    case F(V, V1) of
-        {left,V2}  -> find_entry2(F, I, V2, {[],T}, Flag);
-        {right,V2} -> find_entry2(F, I, V2, {[],T}, anonym)
+find_entry2(_, Id, Value, {[], []}, anonym) -> {anonym, Id , Value};
+find_entry2(_, Id, Value, {[], []}, id) -> {id, Id, Value};
+find_entry2(F, Id, Value, {[], [Values1 | T]}, Flag) ->
+    case F(Value, Values1) of
+        {left,Values2}  -> find_entry2(F, Id, Values2, {[],T}, Flag);
+        {right,Values2} -> find_entry2(F, Id, Values2, {[],T}, anonym)
     end;
-find_entry2(F, I, V, {[{_, _, []} | T], Vs}, Flag) -> find_entry2(F, I, V, {T, Vs}, Flag);
-find_entry2(F, I, V, {[{I1, _, [V1|_]} | T], Vs}, Flag) -> 
-    case F(V, V1) of
-        {left,V2}  -> find_entry2(F, I, V2, {T, Vs}, Flag);
-        {right,V2} -> find_entry2(F, I1, V2, {T, Vs}, Flag)
+find_entry2(F, Id, Value, {[{_, _, []} | T], Values}, Flag) -> find_entry2(F, Id, Value, {T, Values}, Flag);
+find_entry2(F, Id, Value, {[{Id1, _, [Values1|_]} | T], Values}, Flag) -> 
+    case F(Value, Values1) of
+        {left,Values2}  -> find_entry2(F, Id, Values2, {T, Values}, Flag);
+        {right,Values2} -> find_entry2(F, Id1, Values2, {T, Values}, Flag)
     end.
 
 %% Private function
-join_and_replace(Ir, V, C) -> 
+join_and_replace(Ir, Value, Clock) -> 
     [if
-       I == Ir -> {I, N, [V]};
-       true    -> {I, N, []}
+       Id == Ir -> {Id, Counter, [Value]};
+       true    -> {Id, Counter, []}
      end
-     || {I, N, _} <- C].
+     || {Id, Counter, _} <- Clock].
 
 
 %% ===================================================================
@@ -343,7 +343,7 @@ sync_test() ->
     A1  = update(new_list(join(A),[v2]), a),
     A3  = update(new_list(join(A1),[v3]), b),
     A4  = update(new_list(join(A1),[v3]), c),
-    F   = fun (L,R) -> L>R end,
+    F   = fun (ListClocks,R) -> ListClocks>R end,
     W   = {[{a,1,[]}],[]},
     Z   = {[{a,2,[v2,v1]}],[]},
     ?assertEqual( sync([W,Z])     , {[{a,2,[v2]}],[]}                         ),
@@ -395,8 +395,8 @@ lww_last_test() ->
     ok.
 
 reconcile_test() ->
-    F1 = fun (L) -> lists:sum(L) end,
-    F2 = fun (L) -> hd(lists:sort(L)) end,
+    F1 = fun (ListClocks) -> lists:sum(ListClocks) end,
+    F2 = fun (ListClocks) -> hd(lists:sort(ListClocks)) end,
     X  = {[{a,4,[5,2]},{b,1,[]},{c,1,[3]}],[]},
     Y  = {[{a,4,[5,2]},{b,1,[]},{c,1,[3]}],[10,0]},
     ?assertEqual( reconcile(F1,X) , {[{a,4,[]},{b,1,[]},{c,1,[]}],[10]} ),
@@ -410,19 +410,19 @@ less_test() ->
     B  = update(new_list(join(A),[v2]), a),
     B2 = update(new_list(join(A),[v2]), b),
     B3 = update(new_list(join(A),[v2]), z),
-    C  = update(new_list(join(B),[v3]), A, c),
-    D  = update(new_list(join(C),[v4]), B2, d),
+    Clock  = update(new_list(join(B),[v3]), A, c),
+    D  = update(new_list(join(Clock),[v4]), B2, d),
     ?assert(    less(A,B)  ),
-    ?assert(    less(A,C)  ),
-    ?assert(    less(B,C)  ),
+    ?assert(    less(A,Clock)  ),
+    ?assert(    less(B,Clock)  ),
     ?assert(    less(B,D)  ),
     ?assert(    less(B2,D) ),
     ?assert(    less(A,D)  ),
-    ?assertNot( less(B2,C) ),
+    ?assertNot( less(B2,Clock) ),
     ?assertNot( less(B,B2) ),
     ?assertNot( less(B2,B) ),
     ?assertNot( less(A,A)  ),
-    ?assertNot( less(C,C)  ),
+    ?assertNot( less(Clock,Clock)  ),
     ?assertNot( less(D,B2) ),
     ?assertNot( less(B3,D) ),
     ok.
@@ -430,12 +430,12 @@ less_test() ->
 equal_test() ->
     A = {[{a,4,[v5,v0]},{b,0,[]},{c,1,[v3]}], [v0]},
     B = {[{a,4,[v555,v0]}, {b,0,[]}, {c,1,[v3]}], []},
-    C = {[{a,4,[v5,v0]},{b,0,[]}], [v6,v1]},
+    Clock = {[{a,4,[v5,v0]},{b,0,[]}], [v6,v1]},
     % compare only the causal history
     ?assert(    equal(A,B) ),
     ?assert(    equal(B,A) ),
-    ?assertNot( equal(A,C) ),
-    ?assertNot( equal(B,C) ),
+    ?assertNot( equal(A,Clock) ),
+    ?assertNot( equal(B,Clock) ),
     ok.
 
 size_test() ->
@@ -446,13 +446,13 @@ size_test() ->
 ids_values_test() ->
     A = {[{a,4,[v0,v5]},{b,0,[]},{c,1,[v3]}], [v1]},
     B = {[{a,4,[v0,v555]}, {b,0,[]}, {c,1,[v3]}], []},
-    C = {[{a,4,[]},{b,0,[]}], [v1,v6]},
+    Clock = {[{a,4,[]},{b,0,[]}], [v1,v6]},
     ?assertEqual( ids(A)                , [a,b,c]       ),
     ?assertEqual( ids(B)                , [a,b,c]       ),
-    ?assertEqual( ids(C)                , [a,b]         ),
+    ?assertEqual( ids(Clock)                , [a,b]         ),
     ?assertEqual( lists:sort(values(A)) , [v0,v1,v3,v5] ),
     ?assertEqual( lists:sort(values(B)) , [v0,v3,v555]  ),
-    ?assertEqual( lists:sort(values(C)) , [v1,v6]       ),
+    ?assertEqual( lists:sort(values(Clock)) , [v1,v6]       ),
     ok.
 
 map_test() ->
